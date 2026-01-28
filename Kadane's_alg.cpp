0d8363afb5b5abe74d915b5892053ed3dd69effe
Tango <maxim.ant07@gmail.com>
2026-01-28 08:07:17 +0300

    Update Kadane's_alg.cpp

--- Kadane's_alg.cpp
+++ Kadane's_alg.cpp
@@ -37,3 +37,30 @@ int32_t maxProfit(std::vector<int32_t>& pPrices)
 	return maxProfit;
 }
 };
+
+//152. Maximum Product Subarray
+class Solution {
+public:
+int32_t maxProduct(const std::vector<int32_t>& pNumbers)
+{
+	if (pNumbers.empty())
+		return 0;
+
+	int32_t minProduct = pNumbers[0];
+	int32_t maxProduct = pNumbers[0];
+	int32_t result = pNumbers[0];
+
+	for (size_t i = 1; i < pNumbers.size(); ++i)
+	{
+		if (pNumbers[i] < 0)
+			std::swap(minProduct, maxProduct);
+
+		minProduct = std::min(pNumbers[i], minProduct * pNumbers[i]);
+		maxProduct = std::max(pNumbers[i], maxProduct * pNumbers[i]);
+
+		result = std::max(result, maxProduct);
+	}
+
+	return result;
+}
+};
