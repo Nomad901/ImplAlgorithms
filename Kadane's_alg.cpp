18b436bd3f483e87ad1ad2d8863fb72afd1ba66a
Tango <maxim.ant07@gmail.com>
2026-01-28 07:49:03 +0300

    Update Kadane's_alg.cpp

--- Kadane's_alg.cpp
+++ Kadane's_alg.cpp
@@ -14,3 +14,26 @@ int32_t maxSubArray(const std::vector<int32_t>& pNumbers)
 }
 
 };
+
+
+// 121. Best Time to Buy and Sell Stock
+class Solution {
+public:
+int32_t maxProfit(std::vector<int32_t>& pPrices)
+{
+	if (pPrices.size() < 2)
+		return 0;
+
+	int32_t currentProfit = 0, maxProfit = 0;
+
+	for (size_t i = 1; i < pPrices.size(); ++i)
+	{
+		int32_t dailyProfit = pPrices[i] - pPrices[i - 1];
+
+		currentProfit = std::max(dailyProfit, dailyProfit + currentProfit);
+		maxProfit = std::max(currentProfit, maxProfit);
+	}
+
+	return maxProfit;
+}
+};
